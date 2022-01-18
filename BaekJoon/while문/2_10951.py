02f419c11208821fb8ea98b30458e760d2205e0c
Choi A Yeong <cdd8816@gmail.com>
2022-01-18 17:06:54 +0900

    A+B - 4

---
+++ BaekJoon/while문/2_10951.py
@@ -0,0 +1,13 @@
+import sys
+
+def main():
+    while True:
+        try:
+            a, b = map(int, input().split())
+            print(a + b)
+        except:
+            break
+
+if __name__ == "__main__":
+    main()
+    
