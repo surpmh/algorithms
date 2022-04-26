0ee77fa92a3a65a9eb5fbdd79b7a18fd09f5b4e7
surpmh <cdd8816@gmail.com>
2022-04-26 14:26:54 +0900

    손익분기점

---
+++ BaekJoon/기본 수학 1/1_1712.py
@@ -0,0 +1,11 @@
+def main():
+    A, B, C = map(int, input().split())
+    num = 1
+
+    if B >= C:
+        print(-1)
+    else:
+        print(A // (C - B) + 1)
+
+if __name__ == "__main__":
+    main()
