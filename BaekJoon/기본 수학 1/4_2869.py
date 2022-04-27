c8c59837053f38bd43f34f9e2e0ecfebf1909b78
surpmh <cdd8816@gmail.com>
2022-04-27 13:44:20 +0900

    달팽이는 올라가고 싶다

--- BaekJoon/기본 수학 1/4_2869.py
+++ BaekJoon/기본 수학 1/4_2869.py
@@ -1,8 +1,12 @@
-A, B, V = map(int, input().split())
+def main():
+    A, B, V = map(int, input().split())
 
-result = (V - B) // (A - B)
+    result = (V - B) // (A - B)
 
-if (V - B) % (A - B) == 0:
-    print(result)
-else:
-    print(result + 1)
+    if (V - B) % (A - B) == 0:
+        print(result)
+    else:
+        print(result + 1)
+
+if __name__ == "__main__":
+    main()
