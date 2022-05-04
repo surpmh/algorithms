77a72feed3ecbc0985f01c3699887c9a33955349
surpmh <cdd8816@gmail.com>
2022-05-04 10:12:26 +0900

    덩치

---
+++ BaekJoon/브루트 포스/3_7568.py
@@ -0,0 +1,20 @@
+# 덩치
+def main():
+    n = int(input())
+    bulk = []
+
+    for i in range(n):
+        bulk.append(list(map(int,input().split())))
+
+    for i in range(n):
+        rank = 1
+        for j in range(n):
+            if i == j:
+                continue
+            if bulk[i][0] < bulk[j][0] and bulk[i][1] < bulk[j][1]:
+                rank += 1
+        
+        print(rank, end=' ')
+
+if __name__ == "__main__":
+    main()
