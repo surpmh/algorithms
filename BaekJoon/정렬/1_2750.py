b2ed7d670834b44b5dfaa4a0effca715e54e9b49
surpmh <cdd8816@gmail.com>
2022-05-07 08:49:34 +0900

    수 정렬하기

---
+++ BaekJoon/정렬/1_2750.py
@@ -0,0 +1,15 @@
+# 수 정렬하기
+def main():
+    n = int(input())
+    num = []
+
+    for i in range(n):
+        num.append(int(input()))
+
+    num.sort()
+
+    for i in range(len(num)):
+        print(num[i])
+
+if __name__ == "__main__":
+    main()
