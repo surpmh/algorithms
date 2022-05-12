83459fcb1a8d44e8e9d35e6d8687ffc9ddb1d0e2
surpmh <cdd8816@gmail.com>
2022-05-12 13:34:11 +0900

    최대공약수와 최소공배수

---
+++ BaekJoon/정수론 및 조합론/3_2609.py
@@ -0,0 +1,22 @@
+# 최대공약수와 최소공배수
+import sys
+input = sys.stdin.readline
+def gcd(a, b):
+    if a % b == 0:
+        return b
+
+    return gcd(b, a % b)
+
+def main():
+    n1, n2 = map(int, input().split())
+
+    if n1 < n2:
+        n1, n2 = n2, n1
+
+    num = gcd(n1, n2)
+
+    print(num)
+    print((n1 * n2) // num)
+    
+if __name__ == "__main__":
+    main()
