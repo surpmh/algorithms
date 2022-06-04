6bacdefb479721136a1cfcba445a1f8ab0d843b4
surpmh <cdd8816@gmail.com>
2022-06-04 19:23:08 +0900

    제로

---
+++ BaekJoon/스택/2_10773.py
@@ -0,0 +1,16 @@
+# 제로
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+arr = []
+
+for _ in range(k):
+    n = int(input())
+
+    if n == 0:
+        arr.pop()
+    else:
+        arr.append(n)
+
+print(sum(arr))
