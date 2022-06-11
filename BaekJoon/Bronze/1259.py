98227661f32e7c88408bdd2af831021b64a8c05f
surpmh <cdd8816@gmail.com>
2022-06-11 13:15:21 +0900

    팰린드롬수

---
+++ BaekJoon/Bronze/1259.py
@@ -0,0 +1,14 @@
+# 팰린드롬수
+import sys
+input = sys.stdin.readline
+
+while True:
+    n = input().rstrip()
+
+    if n == '0':
+        break
+
+    if n == n[::-1]:
+        print("yes")
+    else:
+        print("no")
