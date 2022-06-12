ee943b18ac725d6dd5f781ed189b31de4f1b7938
surpmh <cdd8816@gmail.com>
2022-06-12 12:01:23 +0900

    덱

---
+++ BaekJoon/큐/5_10866.py
@@ -0,0 +1,42 @@
+# 덱
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+d = deque()
+
+for i in range(n):
+    command = input().rstrip().split()
+
+    if command[0] == 'push_front':
+        d.appendleft(command[1])
+    elif command[0] == 'push_back':
+        d.append(command[1])
+    elif command[0] == 'pop_front':
+        if d:
+            print(d.popleft())
+        else:
+            print(-1)
+    elif command[0] == 'pop_back':
+        if d:
+            print(d.pop())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(d))
+    elif command[0] == "empty":
+        if d:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'front':
+        if d:
+            print(d[0])
+        else:
+            print(-1)
+    elif command[0] == 'back':
+        if d:
+            print(d[-1])
+        else:
+            print(-1)
