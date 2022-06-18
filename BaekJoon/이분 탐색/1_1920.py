c08a9a710dc8b85eb164b75a57a5b942638d8caa
surpmh <cdd8816@gmail.com>
2022-06-18 17:02:13 +0900

    수 찾기

---
+++ BaekJoon/이분 탐색/1_1920.py
@@ -0,0 +1,28 @@
+# 수 찾기
+from array import array
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+m = int(input())
+b = list(map(int, input().split()))
+
+for i in range(m):
+    start = 0
+    end = n-1
+    mid = 0
+    result = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+        if a[mid] == b[i]:
+            result = 1
+            break
+        elif a[mid] < b[i]:
+            start = mid + 1
+        elif a[mid] > b[i]:
+            end = mid - 1
+    
+    print(result)
