69d46dd2daf38a48b9998e67dd0e761ca0bb6dc7
surpmh <cdd8816@gmail.com>
2022-06-20 21:25:51 +0900

    랜선 자르기

---
+++ BaekJoon/이분 탐색/2_1654.py
@@ -0,0 +1,28 @@
+# 랜선 자르기
+import sys
+input = sys.stdin.readline
+
+
+k, n = map(int, input().split())
+online = []
+
+for _ in range(k):
+    online.append(int(input()))
+
+start = 0
+end = sum(online) // n
+
+while start <= end:
+    lines = 0
+    mid = (start + end) // 2
+    if mid == 0:
+        mid = 1
+    for i in range(k):
+        lines += online[i] // mid
+
+    if lines >= n:
+        start = mid + 1
+    elif lines < n:
+        end = mid - 1
+
+print(end)
