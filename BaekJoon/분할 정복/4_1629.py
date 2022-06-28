1fdd603d6612ad32fa645eed81bb9df253c73a57
surpmh <cdd8816@gmail.com>
2022-06-28 20:13:38 +0900

    곱셈

---
+++ BaekJoon/분할 정복/4_1629.py
@@ -0,0 +1,18 @@
+# 곱셈
+import sys
+input = sys.stdin.readline
+
+def power(a, b):
+    if b == 1:
+        return a % c
+    
+    temp = power(a, b // 2)
+
+    if b % 2 == 0:
+        return temp * temp % c
+    else:
+        return temp * temp * a % c
+
+a, b, c = map(int, input().split())
+
+print(power(a, b))
