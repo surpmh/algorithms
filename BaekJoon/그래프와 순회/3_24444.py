4c20858c5d0d9e65db85ccfd097c0ef22d1b7610
surpmh <cdd8816@gmail.com>
2022-07-08 21:08:01 +0900

    알고리즘 수업 - 너비 우선 탐색 1

---
+++ BaekJoon/그래프와 순회/3_24444.py
@@ -0,0 +1,34 @@
+# 알고리즘 수업 - 너비 우선 탐색 2
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(v, order):
+    q.append(v)
+    visited[v] = order
+
+    while q:
+        v = q.popleft()
+        for i in graph[v]:
+            if visited[i] == 0:
+                q.append(i)
+                order += 1
+                visited[i] = order
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+q = deque([])
+
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for g in graph:
+    g.sort()
+
+bfs(r, 1)
+
+for i in range(1, n+1):
+    print(visited[i])
