ae94b58f04a1411d679f0d86381bfee34f7fe6a6
surpmh <cdd8816@gmail.com>
2022-07-12 22:49:51 +0900

    가장 긴 증가하는 부분 수열

---
+++ BaekJoon/동적 계획법 1/12_11053.py
@@ -0,0 +1,14 @@
+# 가장 긴 증가하는 부분 수열
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+p = list(map(int, input().split()))
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if p[i] > p[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
