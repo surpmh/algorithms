2191ee79deacf8ac090d187bbdf64797f65df664
surpmh <cdd8816@gmail.com>
2022-07-13 20:27:48 +0900

    최대 힙

---
+++ BaekJoon/우선순위 큐/1_11279.py
@@ -0,0 +1,18 @@
+# 최대 힙
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    x = int(input())
+
+    if x == 0:
+        if not heap:
+            print(0)
+        else:
+            print(-heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, -x)
