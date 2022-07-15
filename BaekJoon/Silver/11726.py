56102f28318741f34fc41aea99f829f198b4de83
surpmh <cdd8816@gmail.com>
2022-07-15 21:25:27 +0900

    2×n 타일링

--- BaekJoon/Silver/11726.py
+++ BaekJoon/Silver/11726.py
@@ -1,32 +1,12 @@
-# 연결 요소의 개수
+# 2×n 타일링
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-def bfs(v):
-    q = deque([v])
-    visited[v] = True
+n = int(input())
+dp = [0, 1, 2]
 
-    while q:
-        v = q.popleft()
-        for i in graph[v]:
-            if visited[i] == False:
-                visited[i] = True
-                q.append(i)
+if n > 2:
+    for i in range(3, n+1):
+        dp.append(dp[i-2] + dp[i-1])
 
-n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
-count = 0
-
-for i in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-
-for i in range(1, n+1):
-    if visited[i] == False:
-        bfs(i)
-        count += 1
-
-print(count)
+print(dp[n] % 10007)
