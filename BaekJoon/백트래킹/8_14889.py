8a00050fd3e78d731dd4380db4fc5118f2ad92f6
surpmh <cdd8816@gmail.com>
2022-07-17 21:07:25 +0900

    스타트와 링크

--- BaekJoon/백트래킹/8_14889.py
+++ BaekJoon/백트래킹/8_14889.py
@@ -1,15 +1,33 @@
-#스타트와 링크
+# 스타트와 링크
 import sys
 inupt = sys.stdin.readline
 
+def dfs(depth, idx):
+    global result
+
+    if depth == n // 2:
+        start, link = 0, 0
+        for i in range(n):
+            for j in range(n):
+                if visited[i] and visited[j]:
+                    start += graph[i][j]
+                elif not visited[i] and not visited[j]:
+                    link += graph[i][j]
+
+        result = min(result, abs(start - link))
+        return
+
+    for i in range(idx, n):
+        if not visited[i]:
+            visited[i] = True
+            dfs(depth+1, i+1)
+            visited[i] = False
+
 n = int(input())
-s = [list(map(int, input().split())) for i in range(n)]
-value = []
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [False] * n
+result = sys.maxsize
 
-#def dfs(i, j):
+dfs(0, 0)
 
-
-for i in range(n // 2 - 1):
-    for j in range(i+1, n):
-        print(i, j)
-
+print(result)
