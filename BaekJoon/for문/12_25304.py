ffa1d8ecf98917b8645a4b28d1659fbd838600cf
surpmh <cdd8816@gmail.com>
2022-08-13 12:09:15 +0900

    영수증

---
+++ BaekJoon/for문/12_25304.py
@@ -0,0 +1,17 @@
+# 영수증
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+n = int(input())
+sum = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+
+    sum += a * b
+
+if x == sum:
+    print('Yes')
+else:
+    print('No')
