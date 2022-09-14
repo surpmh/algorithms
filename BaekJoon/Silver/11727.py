7352837ad419f64a21cbdb5bb2e1f1a9ccf80ba6
surpmh <cdd8816@gmail.com>
2022-09-14 13:55:20 +0900

    2×n 타일링 2

---
+++ BaekJoon/Silver/11727.py
@@ -0,0 +1,11 @@
+# 2×n 타일링 2
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0, 1, 3]
+
+for i in range(3, 1001):
+    dp.append((dp[i - 2] * 2) + dp[i - 1])
+
+print(dp[n] % 10007)
