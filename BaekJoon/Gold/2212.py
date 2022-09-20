03a36ffb9fde459bb403fdc23fd90b1036bb0d0f
surpmh <cdd8816@gmail.com>
2022-09-20 20:53:12 +0900

    센서

---
+++ BaekJoon/Gold/2212.py
@@ -0,0 +1,16 @@
+# 센서
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+sensors = sorted(list(map(int, input().split())))
+
+d = []
+for i in range(1, n):
+    d.append(sensors[i] - sensors[i-1])
+
+
+d.sort()
+
+print(sum(d[:(n-k)]))
