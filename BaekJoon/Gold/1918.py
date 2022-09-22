2fefbc1c8a457e77879b5802d4f2d1e00357c26b
surpmh <cdd8816@gmail.com>
2022-09-22 20:17:19 +0900

    후위 표기식

---
+++ BaekJoon/Gold/1918.py
@@ -0,0 +1,31 @@
+# 후위 표기식
+import sys
+input = sys.stdin.readline
+
+str = input().rstrip()
+stack = []
+result = ''
+
+for s in str:
+    if s.isalpha():
+        result += s
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                result += stack.pop()
+            stack.append(s)
+        elif s == '+' or s == '-':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(s)
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
+
+while stack:
+    result += stack.pop()
+
+print(result)
