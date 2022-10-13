38eee23e5b1d809b82eb93c12d4e96cf1c937095
surpmh <cdd8816@gmail.com>
2022-10-13 22:43:18 +0900

    에너지 모으기

---
+++ BaekJoon/Silver/16198.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(x):
+    global answer
+    if len(w) == 2:
+        answer = max(answer, x)
+        return answer
+    
+    for i in range(1, len(w)-1):
+        e = w[i-1] * w[i+1]
+        v = w.pop(i)
+        dfs(x + e)
+        w.insert(i, v)
+
+n = int(input())
+w = list(map(int, input().split()))
+answer = 0
+
+dfs(0)
+print(answer)
