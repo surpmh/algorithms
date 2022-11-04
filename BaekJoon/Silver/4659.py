8dc98d44000114783feb403d4bf6aa714e928322
surpmh <cdd8816@gmail.com>
2022-11-04 12:06:43 +0900

    비밀번호 발음하기

--- BaekJoon/Silver/4659.py
+++ BaekJoon/Silver/4659.py
@@ -12,7 +12,7 @@ while True:
     case1 = len(re.findall('[aeiou]',s)) != 0
     case2 = len(re.findall('[aeiou]{3}|[^aeiou]{3}',s)) == 0
     case3 = len(re.findall('([a-df-np-z])\\1',s)) == 0
-    
+
     if case1 and case2 and case3:
         print("<" + s + "> is acceptable.")
     else:
