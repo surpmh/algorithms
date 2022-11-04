0a1752d4871af93af79f0d8a7a352bd03762141c
surpmh <cdd8816@gmail.com>
2022-11-04 12:06:16 +0900

    비밀번호 발음하기

---
+++ BaekJoon/Silver/4659.py
@@ -0,0 +1,19 @@
+# 비밀번호 발음하기
+import sys
+import re
+input = sys.stdin.readline
+
+while True:
+    s = input().rstrip()
+
+    if s == "end":
+        break
+
+    case1 = len(re.findall('[aeiou]',s)) != 0
+    case2 = len(re.findall('[aeiou]{3}|[^aeiou]{3}',s)) == 0
+    case3 = len(re.findall('([a-df-np-z])\\1',s)) == 0
+    
+    if case1 and case2 and case3:
+        print("<" + s + "> is acceptable.")
+    else:
+        print("<" + s + "> is not acceptable.")
