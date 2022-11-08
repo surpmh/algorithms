8364b5900223cb117be37c11a8bb3d888e1f5337
surpmh <cdd8816@gmail.com>
2022-11-08 20:33:30 +0900

    게임

---
+++ BaekJoon/Silver/1072.py
@@ -0,0 +1,27 @@
+# 게임
+import sys
+import math
+input = sys.stdin.readline
+
+x, y = map(int, input().split())
+z = y * 100 // x
+answer = 0
+
+if z >= 99:
+    answer = -1
+else:
+    start = 1
+    end = x
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        print(start, mid, end, (y + mid) * 100 // (x + mid))
+
+        if (y + mid) * 100 // (x + mid) <= z:
+            start = mid + 1
+        else:
+            answer = mid
+            end = mid - 1
+
+print(answer)
