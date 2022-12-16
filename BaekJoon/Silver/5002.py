26d21677dea955cbb43563be00553692e94b77b5
surpmh <cdd8816@gmail.com>
2022-12-16 23:15:42 +0900

    도어맨

--- BaekJoon/Silver/5002.py
+++ BaekJoon/Silver/5002.py
@@ -1,11 +1,43 @@
-# 대표 자연수
+# 도어맨
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
-arr = sorted(list(map(int, input().split())))
+x = int(input())
+line = deque(list(input().rstrip()))
+wait = []
+m = 0
+w = 0
 
-if (n % 2) == 0:
-    print(arr[n // 2 - 1])
-else:
-    print(arr[n // 2])
+while line:
+    if wait:
+        gender = wait.pop()
+        if gender == 'M':
+            m += 1
+        else:
+            w += 1
+    else:
+        gender = line[0]
+
+        if gender == 'M':
+            if abs((m + 1) - w) <= x:
+                line.popleft()
+                m += 1
+            elif len(line) > 2 and line[1] == 'W':
+                wait.append(line.popleft())
+                line.popleft()
+                w += 1
+            else:
+                break
+        else:
+            if abs((w + 1) - m) <= x:
+                line.popleft()
+                w += 1
+            elif len(line) > 2 and line[1] == 'M':
+                wait.append(line.popleft())
+                line.popleft()
+                m += 1
+            else:
+                break
+
+print(m+w)
