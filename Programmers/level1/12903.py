8acd5fd197f3dcd67ca9ec6759e364478e514b23
surpmh <cdd8816@gmail.com>
2023-01-10 19:53:43 +0900

    가운데 글자 가져오기

---
+++ Programmers/level1/12903.py
@@ -0,0 +1,10 @@
+# 가운데 글자 가져오기
+def solution(s):
+    if len(s) % 2 == 0:
+        answer = s[len(s) // 2 - 1] + s[len(s) // 2]
+    else:
+        answer = s[len(s) // 2]
+        
+    return answer
+
+print(solution("abcde"))
