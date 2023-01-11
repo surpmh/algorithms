2d91e872ea8fdaa31faa61a119e036656121e445
surpmh <cdd8816@gmail.com>
2023-01-11 20:48:01 +0900

    최솟값 만들기

---
+++ Programmers/level2/12941.py
@@ -0,0 +1,13 @@
+# 최솟값 만들기
+def solution(A,B):
+    answer = 0
+
+    A.sort()
+    B.sort(reverse=True)
+
+    for a, b in zip(A, B):
+        answer += a * b
+
+    return answer
+
+print(solution([1, 4, 2], [5, 4, 4]))
