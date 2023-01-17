b4aa7e5a064daa1fb9d6247dae660dfee62d04b8
surpmh <cdd8816@gmail.com>
2023-01-17 22:58:31 +0900

    주식가격

---
+++ Programmers/level2/42584.py
@@ -0,0 +1,15 @@
+# 주식가격
+def solution(prices):
+    answer = [0] * len(prices)
+
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+
+    return answer
+
+print(solution([1, 2, 3, 2, 3]))
