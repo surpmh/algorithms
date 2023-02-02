8cd4da4eca9770b19e1d760db2da9e1f4bce0efd
surpmh <cdd8816@gmail.com>
2023-02-02 21:03:48 +0900

    옹알이

---
+++ Programmers/level1/133499.py
@@ -0,0 +1,25 @@
+# 옹알이
+def solution(babbling):
+    pronunciation = ["aya", "ye", "woo", "ma"]
+    answer = 0
+
+    for word in babbling:
+        say = ""
+        before = ""
+        for alphabet in word:
+            say += alphabet
+
+            if say == before:
+                continue
+
+            if say in pronunciation:
+                before = say
+                say = ""
+
+        if say == "":
+            answer += 1
+
+            
+    return answer
+
+print(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))
