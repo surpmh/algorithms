0f03c03e3e7115e2f698c284fa287d472905476b
surpmh <cdd8816@gmail.com>
2023-02-03 21:56:45 +0900

    피보나치 수

---
+++ Programmers/level2/12945.py
@@ -0,0 +1,15 @@
+# 피보나치 수
+def solution(n):
+    dp = [0] * 100001
+    dp[0] = 0
+    dp[1] = 1
+
+    if n < 2:
+        return n
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+
+    return dp[n]
+
+print(solution(3))
