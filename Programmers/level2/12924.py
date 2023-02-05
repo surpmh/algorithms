cff5c58c21bdae6d34f60cd4d8c706583f1f2372
surpmh <cdd8816@gmail.com>
2023-02-05 12:04:55 +0900

    숫자의 표현

---
+++ Programmers/level2/12924.py
@@ -0,0 +1,18 @@
+# 숫자의 표현
+def solution(n):
+    answer = 1
+
+    for i in range(1, n):
+        sum = 0
+        for j in range(i, n):
+            sum += j
+
+            if sum == n:
+                answer += 1
+                break
+            elif sum > n:
+                break
+
+    return answer
+
+print(solution(15))
