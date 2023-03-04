7c484ebb06a4fdf759a7f4e85544fad0917f636a
surpmh <cdd8816@gmail.com>
2023-03-04 21:13:49 +0900

    124 나라의 숫자

---
+++ Programmers/level2/12899.py
@@ -0,0 +1,15 @@
+# 124 나라의 숫자
+def solution(n):
+    answer = ''
+
+    while n:
+        if n % 3:
+            answer += str(n % 3)
+            n //= 3
+        else:
+            answer += "4"
+            n = n//3 - 1
+
+    return answer[::-1]
+
+print(solution(1))
