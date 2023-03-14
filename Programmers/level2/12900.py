df0103304d7c2d94a265d2cc1677e4245e4b4b6a
surpmh <cdd8816@gmail.com>
2023-03-14 18:45:41 +0900

    2 x n 타일링

---
+++ Programmers/level2/12900.py
@@ -0,0 +1,11 @@
+# 2 x n 타일링
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 1, 2
+
+    for i in range(3, n+1):
+        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007
+
+    return dp[n]
+
+print(solution(4))
