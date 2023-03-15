5c293c021d04a6ad334718c2d02541a6a3e76818
surpmh <cdd8816@gmail.com>
2023-03-15 21:41:57 +0900

    신고 결과 받기

---
+++ Programmers/level2/92334.py
@@ -0,0 +1,26 @@
+# 신고 결과 받기
+def solution(id_list, report, k):
+    answer = [0 for _ in range(len(id_list))]
+    result = [0 for _ in range(len(id_list))]
+    
+    report = set(report)
+    report = list(report)
+    
+    for i in range(len(report)):
+        a, b = report[i].split()
+
+        answer[id_list.index(b)] += 1
+        
+    for i in range(len(answer)):
+        if answer[i] < k:
+            answer[i] = 0
+
+    for i in range(len(report)):
+        a, b = report[i].split()
+
+        if answer[id_list.index(b)] >= k:
+            result[id_list.index(a)] += 1
+            
+    return result
+
+print(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
