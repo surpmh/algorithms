9391a55e17e0b538e557e326804c04b3a815fee8
surpmh <cdd8816@gmail.com>
2023-03-18 21:03:54 +0900

    n^2 배열 자르기

---
+++ Programmers/level2/87390.py
@@ -0,0 +1,10 @@
+# n^2 배열 자르기
+def solution(n, left, right):
+    answer = []
+
+    for i in range(left, right + 1):
+        answer.append(max(i // n, i % n) + 1)
+
+    return answer
+
+print(solution(3, 2, 5))
