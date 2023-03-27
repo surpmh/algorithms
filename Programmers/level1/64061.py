bf17ffee14b98bd3a5e3d5723455829b00d452ab
surpmh <cdd8816@gmail.com>
2023-03-27 22:38:59 +0900

    크레인 인형뽑기 게임

--- Programmers/level1/64061.py
+++ Programmers/level1/64061.py
@@ -2,7 +2,7 @@
 def solution(board, moves):
     answer = 0
     stack = [0]
-    
+
     for move in moves:
         for row in board:
             if row[move-1]:
@@ -15,7 +15,7 @@ def solution(board, moves):
                 else:
                     stack.append(num)
                 break
-
+            
     return answer
 
 print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
