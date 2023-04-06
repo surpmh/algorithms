9f05185f4782364189355831d926c72fee467b33
surpmh <cdd8816@gmail.com>
2023-04-06 23:13:06 +0900

    수학은 비대면강의입니다

---
+++ BaekJoon/브루트 포스/6_19532.py
@@ -0,0 +1,11 @@
+# 수학은 비대면강의입니다
+def main():
+    a, b, c, d, e, f = map(int, input().split())
+
+    for x in range(-999, 1000):
+        for y in range(-999, 1000):
+            if a * x + b * y == c and d * x + e * y == f:
+                print(x, y)
+
+if __name__ == "__main__":
+    main()
