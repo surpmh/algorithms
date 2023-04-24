a9e78c744c395404302c1a1352e092fbc2afc811
surpmh <cdd8816@gmail.com>
2023-04-24 23:06:09 +0900

    롤케이크 자르기

---
+++ Programmers/level2/132265.py
@@ -0,0 +1,25 @@
+# 롤케이크 자르기
+from collections import Counter
+
+def solution(topping):
+    answer = 0
+    me = Counter(topping)
+    bro = {}
+    
+    for i in range(len(topping)) :
+        if topping[i] in bro :
+            bro[topping[i]] += 1
+        else :
+            bro[topping[i]] = 1
+
+        me[topping[i]] -= 1
+        
+        if me[topping[i]] == 0 :
+            del me[topping[i]]
+        
+        if len(bro) == len(me) :
+            answer += 1
+    
+    return answer
+
+print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
