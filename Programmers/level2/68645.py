6de766522a10180d128ff1414e8e51099ea53c52
surpmh <cdd8816@gmail.com>
2023-04-26 22:56:49 +0900

    삼각 달팽이

---
+++ Programmers/level2/68645.py
@@ -0,0 +1,23 @@
+# 삼각 달팽이
+def solution(n):
+    answer = [[0 for _ in range(1, i+1)] for i in range(1, n+1)]
+
+    x, y = -1, 0
+    num = 1
+
+    for i in range(n):
+        for _ in range(i, n):
+            if i % 3 == 0:
+                x += 1
+            elif i % 3 == 1:
+                y += 1
+            elif i % 3 == 2:
+                x -= 1
+                y -= 1
+
+            answer[x][y] = num
+            num += 1
+
+    return sum(answer, [])
+
+print(solution(4))
