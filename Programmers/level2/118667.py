6471166e35331375c560d7b0b92a014f006e5fcc
surpmh <cdd8816@gmail.com>
2023-05-07 22:48:21 +0900

    두 큐 합 같게 만들기

---
+++ Programmers/level2/118667.py
@@ -0,0 +1,33 @@
+# 두 큐 합 같게 만들기
+from collections import deque
+
+def solution(queue1, queue2):
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    sum1 = sum(queue1)
+    sum2 = sum(queue2)
+    answer = 0
+
+    if (sum1 + sum2) % 2 == 1:
+        return -1
+    
+    for i in range((sum1 + sum2)):
+        if i == 600000:
+            break
+        if sum1 < sum2:
+            num = queue2.popleft()
+            queue1.append(num)c
+            sum1 += num
+            sum2 -= num
+        elif sum1 > sum2:
+            num = queue1.popleft()
+            queue2.append(num)
+            sum1 -= num
+            sum2 += num
+        else:
+            return answer
+        answer += 1
+        
+    return -1
+
+print(solution([1, 2, 4], [3, 2, 4]))
