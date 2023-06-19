3061662631767efc492eb032c279307e30c02894
surpmh <cdd8816@gmail.com>
2023-06-19 20:38:48 +0900

    과제 진행하기

--- Programmers/level2/176962.py
+++ Programmers/level2/176962.py
@@ -38,4 +38,4 @@ def solution(plans):
 
     return answer
 
-print(solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]))
+print(solution([["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]]))
